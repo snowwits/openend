18468e69f2e6f43656d3bd85bd562fbf2202b0b6
snowwits <snowwits@gmail.com>
2017-03-30 14:44:36 +0200

    Cleaned up JS, Not seek if invalid input;
    Cleaned up JS: Using const and let instead of var;
    Not seek if invalid input: If there is invalid input in the seek amount
    input text field, no seeking will be done;

--- twitch.tv.js
+++ twitch.tv.js
@@ -1,31 +1,32 @@
-var AVAILIBILITY_CHECK_INTERVAL = 200;
-var AVAILIBILITY_CHECK_TIMEOUT = 30000;
-var DEFAULT_SEEK_AMOUNT = 10.0;
-var PROGRESS_TOTAL_TIME_DIV_CLASS = "player-seek__time--total";
-var PROGRESS_SLIDER_DIV_CLASS = "js-player-slider";
+/* Global values */
+const AVAILIBILITY_CHECK_INTERVAL = 200; // 200ms
+const AVAILIBILITY_CHECK_TIMEOUT = 30000; // 30s
+const PROGRESS_TOTAL_TIME_DIV_CLASS = "player-seek__time--total";
+const PROGRESS_SLIDER_DIV_CLASS = "js-player-slider";
 
-var progressVisible = false;
-var initStartTime = null;
+/* Global variables */
+let GLOBAL_progressVisible = false;
 
+/* Functions */
 function handleToggleProgressAction() {
 	console.log("OPENEND: Handling Toggle Progress action");
 
-	// Toggle 
-	progressVisible = !progressVisible;
+	// Toggle
+	GLOBAL_progressVisible = !GLOBAL_progressVisible;
 	
 	updateProgressVisibility();
 }
 
 /* TOGGLE PROGRESS */
 function updateProgressVisibility() {
-	// Make progress indicators visible / invisible
-	var toggleClasses = [PROGRESS_TOTAL_TIME_DIV_CLASS, PROGRESS_SLIDER_DIV_CLASS];
-	for (i = 0; i < toggleClasses.length; i++) {
-		var toggleClass = toggleClasses[i];
-		var elements = document.getElementsByClassName(toggleClass);
-		for(j = 0; j < elements.length; j++) {
-			var element = elements[j];
-			if(progressVisible) {
+	// Make progress indicators visible / hidden
+	const toggleClasses = [PROGRESS_TOTAL_TIME_DIV_CLASS, PROGRESS_SLIDER_DIV_CLASS];
+	for (let i = 0; i < toggleClasses.length; i++) {
+		const toggleClass = toggleClasses[i];
+		const elements = document.getElementsByClassName(toggleClass);
+		for (let j = 0; j < elements.length; j++) {
+			const element = elements[j];
+			if (GLOBAL_progressVisible) {
 				element.style.display = "block";
 			}
 			else {
@@ -35,9 +36,9 @@ function updateProgressVisibility() {
 	}	
 	
 	// Update the button label
-	var oe_toggleProgressBtn = document.getElementById("oe-toggle-progress");
-	var oe_toggleProgressBtnLabelMsg = progressVisible ? "toggleProgress_visible" : "toggleProgress_hidden";
-	oe_toggleProgressBtn.innerHTML = chrome.i18n.getMessage(oe_toggleProgressBtnLabelMsg);
+	const toggleProgressBtn = document.getElementById("oe-toggle-progress");
+	const toggleProgressBtnLabelMsg = GLOBAL_progressVisible ? "toggleProgress_visible" : "toggleProgress_hidden";
+	toggleProgressBtn.innerHTML = chrome.i18n.getMessage(toggleProgressBtnLabelMsg);
 }
 
 /* SEEKING */
@@ -52,42 +53,49 @@ function handleSeekForwardAction() {
 }
 
 function seek(forward = true) {
-	var sliders = document.getElementsByClassName(PROGRESS_SLIDER_DIV_CLASS);
-	if (sliders.length == 1) {
-		var slider = sliders[0];
-		
-		// Get min, max, current time in seconds
-		var minTime = parseInt(slider.getAttribute("aria-valuemin"));
-		var maxTime = parseInt(slider.getAttribute("aria-valuemax"));
-		var currentTime = parseInt(slider.getAttribute("aria-valuenow"));
-				
-		// Get the seek amount in seconds
-		var directionMultiplier = forward ? 1 : -1;
-		var seekAmountInput = document.getElementById("oe-seek-amount");
-		var seekAmount = parseDuration(seekAmountInput.value) * directionMultiplier;
-		
-		// Add the seek amount to the current time (but require: minTime < newTime < maxTime)
-		var newTime = Math.min(maxTime, Math.max(minTime, currentTime + seekAmount));
-		
-		// Build the new url
-		var newTimeFormatted = formatDuration(newTime);
-		var urlParams = newTimeFormatted.length > 0 ? "?t=" + newTimeFormatted : "";
-		var newTimeUrl = window.location.protocol + "//" + window.location.hostname + window.location.pathname + urlParams;
-		
-		console.log("OPENEND: Seeking %is: %is -> %is (%s)", seekAmount, currentTime, newTime, newTimeFormatted);
-		window.location.assign(newTimeUrl);
+	const sliders = document.getElementsByClassName(PROGRESS_SLIDER_DIV_CLASS);
+	if (sliders.length != 1) {
+		console.error("OPENEND: Seeking failed: div.%s not available", PROGRESS_SLIDER_DIV_CLASS);
 	}
+	const slider = sliders[0];
+	
+	// Get min, max, current time in seconds
+	const minTime = parseInt(slider.getAttribute("aria-valuemin"));
+	const maxTime = parseInt(slider.getAttribute("aria-valuemax"));
+	const currentTime = parseInt(slider.getAttribute("aria-valuenow"));
+			
+	// Get the seek amount in seconds
+	const seekDirectionFactor = forward ? 1 : -1;
+	const seekAmountInputValue = document.getElementById("oe-seek-amount").value;
+	const seekAmount = parseDuration(seekAmountInputValue) * seekDirectionFactor;
+	if (seekAmount == 0) {
+		console.log("OPENEND: No valid seek amount input value given: %s", seekAmountInputValue);
+		return;
+	}
+	
+	// Add the seek amount to the current time (but require: minTime < newTime <
+	// maxTime)
+	const newTime = Math.min(maxTime, Math.max(minTime, currentTime + seekAmount));
+	
+	// Build the new url
+	const newTimeFormatted = formatDuration(newTime);
+	const urlParams = newTimeFormatted.length > 0 ? "?t=" + newTimeFormatted : "";
+	const newTimeUrl = window.location.protocol + "//" + window.location.hostname + window.location.pathname + urlParams;
+	
+	console.log("OPENEND: Seeking %is: %is -> %is (%s)", seekAmount, currentTime, newTime, newTimeFormatted);
+	window.location.assign(newTimeUrl);
 }
 
 /*
-	"01h02m03s" -> 1 * 60 * 60 + 2 * 60 + 3 = 3723
-*/
+ * "01h02m03s" -> 1 * 60 * 60 + 2 * 60 + 3 = 3723
+ * 0 if no match
+ */
 function parseDuration(durationString) {
-	var rxTime = new RegExp("(?:(\\d+)h)?(?:(\\d+)m)?(?:(\\d+)s)?");
-	var groups = durationString.match(rxTime);
-	var hours = parseDurationPart(groups, 1);
-	var mins = parseDurationPart(groups, 2);
-	var secs = parseDurationPart(groups, 3);
+	const rxTime = new RegExp("(?:(\\d+)h)?(?:(\\d+)m)?(?:(\\d+)s)?");
+	const groups = durationString.match(rxTime);
+	const hours = parseDurationPart(groups, 1);
+	const mins = parseDurationPart(groups, 2);
+	const secs = parseDurationPart(groups, 3);
 	return secs + mins * 60 + hours * 60 * 60;
 }
 
@@ -96,122 +104,124 @@ function parseDurationPart(groups, index) {
 }
 
 /*
-	3723 = 1 * 60 * 60 + 2 * 60 + 3 -> "01h02m03s"
-*/
+ * 3723 = 1 * 60 * 60 + 2 * 60 + 3 -> "01h02m03s"
+ */
 function formatDuration(duration) {
-	var parts = extractDurationParts(duration);
-	var formatted = "";
+	const parts = extractDurationParts(duration);
+	let formatted = "";
 	if (parts[0] > 0) {
-		formatted += pad(parts[0]) + "h";
+		formatted += padLeft(parts[0]) + "h";
 	}
 	if (parts[1] > 0) {
-		formatted += pad(parts[1]) + "m";
+		formatted += padLeft(parts[1]) + "m";
 	}
 	if (parts[2] > 0) {
-		formatted += pad(parts[2]) + "s";
+		formatted += padLeft(parts[2]) + "s";
 	}
 	return formatted;
 }
 
 /*
-	3723 = 1h, 2m, 3s -> [1, 2, 3]
-*/
+ * 3723 = 1h, 2m, 3s -> [1, 2, 3]
+ */
 function extractDurationParts(duration) {
-	var amount = duration;
-	// calculate (and subtract) whole hours
-	var hours = Math.floor(amount / 3600);
+	let amount = duration;
+	// Calculate (and subtract) whole hours
+	const hours = Math.floor(amount / 3600);
 	amount -= hours * 3600;
 
-	// calculate (and subtract) whole minutes
-	var mins = Math.floor(amount / 60);
+	// Calculate (and subtract) whole minutes
+	const mins = Math.floor(amount / 60);
 	amount -= mins * 60;
 
-	// what's left is seconds
-	var secs = amount % 60;
+	// What's left is seconds
+	const secs = amount % 60;
 	
 	return [hours, mins, secs];
 }
 
-function pad(number, width = 2, padChar = '0') {
-    var padding = new Array(1 + width).join(padChar);
-    return (padding + number).slice(-padding.length);
+function padLeft(number, width = 2, padChar = "0") {
+	let str = number + "";
+	while (str.length < width) {
+		str = padChar + str;
+	}
+	return str;
 }
 
 
 /* INIT */
 function init(){
 	console.log("OPENEND: Initializing...");
-	initStartTime = Date.now();
-	
-	injectUtilSpan();
+	injectUtilSpan(Date.now());
 }
 
-function injectUtilSpan() {
+function injectUtilSpan(initStartTime) {
 	// Inject util span into player seek time container
-	var playerSeekTimeContainers = document.getElementsByClassName("player-seek__time-container");
+	const playerSeekTimeContainers = document.getElementsByClassName("player-seek__time-container");
 	if (playerSeekTimeContainers.length == 1){
-		var playerSeekTimeContainer = playerSeekTimeContainers[0];
-		console.log("OPENEND: div.player-seek__time-container available: %s", playerSeekTimeContainer);
-		oe_utilSpan = buildUtilSpan();
-		playerSeekTimeContainer.appendChild(oe_utilSpan);
-		
+		const playerSeekTimeContainer = playerSeekTimeContainers[0];
+		const utilSpan = buildUtilSpan();
+		playerSeekTimeContainer.appendChild(utilSpan);
+			
 		// Set initial visibility
 		updateProgressVisibility();
+		
+		console.log("OPENEND: Open End utility available (added in div.player-seek__time-container)");
 	} else {
 		if (AVAILIBILITY_CHECK_TIMEOUT > Date.now() - initStartTime) {
-			console.log("OPENEND: div.player-seek__time-container not available yet. Checking again in %ims...", AVAILIBILITY_CHECK_INTERVAL)
-			setTimeout(injectUtilSpan, AVAILIBILITY_CHECK_INTERVAL);	
+			console.log("OPENEND: div to add Open End utility to is not available yet (div.player-seek__time-container). Checking again in %ims...", AVAILIBILITY_CHECK_INTERVAL)
+			setTimeout(injectUtilSpan, AVAILIBILITY_CHECK_INTERVAL, initStartTime);	
 		} else {
-			console.error("OPENEND: Check timeout of %ims reached. Could not add Open End utility to the Twitch page.", AVAILIBILITY_CHECK_TIMEOUT);
+			console.log("OPENEND: Open End utility not available (failed to find div.player-seek__time-container in %ims)", AVAILIBILITY_CHECK_TIMEOUT);
 		}
 	}
 }
 
 function buildUtilSpan() {
 	// Build util span
-	var oe_utilSpan = document.createElement("span");
-	oe_utilSpan.setAttribute("id", "oe-util")
+	const utilSpan = document.createElement("span");
+	utilSpan.setAttribute("id", "oe-util")
 
-	// Build Open End icon img
-	var oe_iconImg = document.createElement("img");
-	var oe_iconImgUrl = chrome.extension.getURL("icon_16.png");
-	oe_iconImg.setAttribute("src", oe_iconImgUrl);
-	// Add "Open End icon" img to util span
-	oe_utilSpan.appendChild(oe_iconImg);
+	// Build Open End img
+	const iconImg = document.createElement("img");
+	const iconImgUrl = chrome.extension.getURL("icon_16.png");
+	iconImg.setAttribute("src", iconImgUrl);
+	// Add "Open End" img to util span
+	utilSpan.appendChild(iconImg);
 	
 	// Build "Toggle Progress" button
-	var oe_toggleProgressBtn = document.createElement("button");
-	oe_toggleProgressBtn.setAttribute("id", "oe-toggle-progress");
+	const toggleProgressBtn = document.createElement("button");
+	toggleProgressBtn.setAttribute("id", "oe-toggle-progress");
 	// innerHTML will be set via updateProgressVisibility()
-	oe_toggleProgressBtn.onclick = handleToggleProgressAction;
-	// Add "Toggle Open End" button to util span
-	oe_utilSpan.appendChild(oe_toggleProgressBtn);
+	toggleProgressBtn.onclick = handleToggleProgressAction;
+	// Add "Toggle Progress" button to util span
+	utilSpan.appendChild(toggleProgressBtn);
 	
 	// Build "Seek Back" button
-	var oe_seekBackBtn = document.createElement("button");
-	oe_seekBackBtn.setAttribute("id", "oe-seek-back");
-	oe_seekBackBtn.innerHTML = "<";
-	oe_seekBackBtn.onclick = handleSeekBackAction;
+	const seekBackBtn = document.createElement("button");
+	seekBackBtn.setAttribute("id", "oe-seek-back");
+	seekBackBtn.innerHTML = "<";
+	seekBackBtn.onclick = handleSeekBackAction;
 	// Add "Seek Back" button to util span
-	oe_utilSpan.appendChild(oe_seekBackBtn);
+	utilSpan.appendChild(seekBackBtn);
 	
 	// Build "Seek Amount" text field
-	var oe_seekAmountInput = document.createElement("input");
-	oe_seekAmountInput.setAttribute("type", "text");
-	oe_seekAmountInput.setAttribute("id", "oe-seek-amount");
-	oe_seekAmountInput.value = "10m";
+	const seekAmountInput = document.createElement("input");
+	seekAmountInput.setAttribute("type", "text");
+	seekAmountInput.setAttribute("id", "oe-seek-amount");
+	seekAmountInput.value = "10m";
 	// Add "Seek Amount" button to util span
-	oe_utilSpan.appendChild(oe_seekAmountInput);
+	utilSpan.appendChild(seekAmountInput);
 	
 	// Build "Seek Forward" button
-	var oe_seekForwardBtn = document.createElement("button");
-	oe_seekForwardBtn.setAttribute("id", "oe-seek-forward");
-	oe_seekForwardBtn.innerHTML = ">";
-	oe_seekForwardBtn.onclick = handleSeekForwardAction;
+	const seekForwardBtn = document.createElement("button");
+	seekForwardBtn.setAttribute("id", "oe-seek-forward");
+	seekForwardBtn.innerHTML = ">";
+	seekForwardBtn.onclick = handleSeekForwardAction;
 	// Add "Seek Forward" button to util span
-	oe_utilSpan.appendChild(oe_seekForwardBtn);
+	utilSpan.appendChild(seekForwardBtn);
 	
-	return oe_utilSpan;
+	return utilSpan;
 }
 
 
