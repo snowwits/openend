3eac92a204c47a8a94f16c69f859796d6807fbf2
snowwits <snowwits@gmail.com>
2017-03-30 11:31:41 +0200

    Added i8n and made a few fine tunings to logging;

--- twitch.tv.js
+++ twitch.tv.js
@@ -4,14 +4,14 @@ var DEFAULT_SEEK_AMOUNT = 10.0;
 var PROGRESS_TOTAL_TIME_DIV_CLASS = "player-seek__time--total";
 var PROGRESS_SLIDER_DIV_CLASS = "js-player-slider";
 
-var showProgress = false;
+var progressVisible = false;
 var initStartTime = null;
 
 function handleToggleProgressAction() {
 	console.log("OPENEND: Handling Toggle Progress action");
 
 	// Toggle 
-	showProgress = !showProgress;
+	progressVisible = !progressVisible;
 	
 	updateProgressVisibility();
 }
@@ -25,7 +25,7 @@ function updateProgressVisibility() {
 		var elements = document.getElementsByClassName(toggleClass);
 		for(j = 0; j < elements.length; j++) {
 			var element = elements[j];
-			if(showProgress) {
+			if(progressVisible) {
 				element.style.display = "block";
 			}
 			else {
@@ -36,7 +36,8 @@ function updateProgressVisibility() {
 	
 	// Update the button label
 	var oe_toggleProgressBtn = document.getElementById("oe-toggle-progress");
-	oe_toggleProgressBtn.innerHTML = showProgress ? "Hide progress" : "Show progress";
+	var oe_toggleProgressBtnLabelMsg = progressVisible ? "toggleProgress_visible" : "toggleProgress_hidden";
+	oe_toggleProgressBtn.innerHTML = chrome.i18n.getMessage(oe_toggleProgressBtnLabelMsg);
 }
 
 /* SEEKING */
@@ -51,10 +52,8 @@ function handleSeekForwardAction() {
 }
 
 function seek(forward = true) {
-	console.log("OPENEND: Handling Seek action");
-	
 	var sliders = document.getElementsByClassName(PROGRESS_SLIDER_DIV_CLASS);
-	if(sliders.length == 1) {
+	if (sliders.length == 1) {
 		var slider = sliders[0];
 		
 		// Get min, max, current time in seconds
@@ -63,22 +62,19 @@ function seek(forward = true) {
 		var currentTime = parseInt(slider.getAttribute("aria-valuenow"));
 				
 		// Get the seek amount in seconds
-		var seekAmountInput = document.getElementById("oe-seek-amount");
-		var seekAmount = parseDuration(seekAmountInput.value);
-		console.log("currentTime: " + currentTime + ", seekAmount: " + seekAmount);
-		
-		// Add the seek amount to the current time
 		var directionMultiplier = forward ? 1 : -1;
-		var newTime = Math.min(maxTime, Math.max(minTime, currentTime + seekAmount * directionMultiplier));
-		console.log("newTime: " + newTime);
+		var seekAmountInput = document.getElementById("oe-seek-amount");
+		var seekAmount = parseDuration(seekAmountInput.value) * directionMultiplier;
+		
+		// Add the seek amount to the current time (but require: minTime < newTime < maxTime)
+		var newTime = Math.min(maxTime, Math.max(minTime, currentTime + seekAmount));
 		
 		// Build the new url
 		var newTimeFormatted = formatDuration(newTime);
-		console.log("newTimeFormatted: " + newTimeFormatted);
 		var urlParams = newTimeFormatted.length > 0 ? "?t=" + newTimeFormatted : "";
 		var newTimeUrl = window.location.protocol + "//" + window.location.hostname + window.location.pathname + urlParams;
 		
-		console.log("Loading new time url: " + newTimeUrl);
+		console.log("OPENEND: Seeking %is: %is -> %is (%s)", seekAmount, currentTime, newTime, newTimeFormatted);
 		window.location.assign(newTimeUrl);
 	}
 }
@@ -142,12 +138,9 @@ function pad(number, width = 2, padChar = '0') {
 }
 
 
-
-
-
 /* INIT */
 function init(){
-	console.log("OPENEND: Initializing");
+	console.log("OPENEND: Initializing...");
 	initStartTime = Date.now();
 	
 	injectUtilSpan();
@@ -158,7 +151,7 @@ function injectUtilSpan() {
 	var playerSeekTimeContainers = document.getElementsByClassName("player-seek__time-container");
 	if (playerSeekTimeContainers.length == 1){
 		var playerSeekTimeContainer = playerSeekTimeContainers[0];
-		console.log("OPENEND: INFO: div.player-seek__time-container available: " + playerSeekTimeContainer);
+		console.log("OPENEND: div.player-seek__time-container available: %s", playerSeekTimeContainer);
 		oe_utilSpan = buildUtilSpan();
 		playerSeekTimeContainer.appendChild(oe_utilSpan);
 		
@@ -166,10 +159,10 @@ function injectUtilSpan() {
 		updateProgressVisibility();
 	} else {
 		if (AVAILIBILITY_CHECK_TIMEOUT > Date.now() - initStartTime) {
-			console.log("OPENEND: DEBUG: div.player-seek__time-container not available yet. Checking again in " + AVAILIBILITY_CHECK_INTERVAL + " ms...")
+			console.log("OPENEND: div.player-seek__time-container not available yet. Checking again in %ims...", AVAILIBILITY_CHECK_INTERVAL)
 			setTimeout(injectUtilSpan, AVAILIBILITY_CHECK_INTERVAL);	
 		} else {
-			console.log("Check timeout of " + AVAILIBILITY_CHECK_TIMEOUT + " ms reached. Could not add open end utility to the twitch page.");
+			console.error("OPENEND: Check timeout of %ims reached. Could not add Open End utility to the Twitch page.", AVAILIBILITY_CHECK_TIMEOUT);
 		}
 	}
 }
@@ -183,13 +176,13 @@ function buildUtilSpan() {
 	var oe_iconImg = document.createElement("img");
 	var oe_iconImgUrl = chrome.extension.getURL("icon_16.png");
 	oe_iconImg.setAttribute("src", oe_iconImgUrl);
-	oe_iconImg.setAttribute("alt", "Open End Chrome Extension")
 	// Add "Open End icon" img to util span
 	oe_utilSpan.appendChild(oe_iconImg);
 	
 	// Build "Toggle Progress" button
 	var oe_toggleProgressBtn = document.createElement("button");
 	oe_toggleProgressBtn.setAttribute("id", "oe-toggle-progress");
+	// innerHTML will be set via updateProgressVisibility()
 	oe_toggleProgressBtn.onclick = handleToggleProgressAction;
 	// Add "Toggle Open End" button to util span
 	oe_utilSpan.appendChild(oe_toggleProgressBtn);
