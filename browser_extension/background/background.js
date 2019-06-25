098e742ce8ef21e58d20681c617be35adb9ea336
snowwits <snowwits@gmail.com>
2019-06-25 19:39:12 +0200

    Implemented real manifest version comparison for migration on update ♻️

--- browser_extension/background/background.js
+++ browser_extension/background/background.js
@@ -31,37 +31,48 @@ function handleInstalled(details) {
     } else if (details.reason === "update") {
         log("Updated Open End from version [%s] to [%s]", details.previousVersion, thisVersion);
 
-        // TODO: Implement a function compareVersions(versionA, versionB)
-        if ("0.3.0" !== details.previousVersion) {
-            migrateFrom_v0_2_1();
+        if (requiresMigration(details.previousVersion, "0.3.0")) {
+            migrateTo_v0_3_0();
         }
     }
 }
 
-function migrateFrom_v0_2_1() {
-    log("Migrating from version 0.2.1 (re-storing options sfmEnabled, sfmChannels under the new keys and in the new format)");
-    const optSfmEnabledGlobalKey_v0_2_1 = "sfmEnabled";
-    const optSfmEnabledChannelsKey_v0_2_1 = "sfmChannels";
-    const optKeys_v0_2_1 = [optSfmEnabledGlobalKey_v0_2_1, optSfmEnabledChannelsKey_v0_2_1];
+/**
+ *
+ * @param previousVersion the previously installed version
+ * @param breakingChangesVersion the version that introduced breaking changes that require migration
+ * @return {boolean} true if migration is needed (previous version lower than breaking changes version or cannot be determined)
+ */
+function requiresMigration(previousVersion, breakingChangesVersion) {
+    const comparisonResult = compareManifestVersions(previousVersion, breakingChangesVersion);
+    return isNaN(comparisonResult) || comparisonResult < 0;
+}
 
-    opnd.browser.readOptions(optKeys_v0_2_1).then((items) => {
+
+function migrateTo_v0_3_0() {
+    log("Migrating to version >=0.3.0 (re-storing options sfmEnabled, sfmChannels under the new keys [%s, %s] and in the new format)", OPT_SFM_ENABLED_GLOBAL_NAME, OPT_SFM_ENABLED_CHANNELS_NAME);
+    const optSfmEnabledGlobalKey_pre_v0_3_0 = "sfmEnabled";
+    const optSfmEnabledChannelsKey_pre_v0_3_0 = "sfmChannels";
+    const optKeys_pre_v0_3_0 = [optSfmEnabledGlobalKey_pre_v0_3_0, optSfmEnabledChannelsKey_pre_v0_3_0];
+
+    opnd.browser.readOptions(optKeys_pre_v0_3_0).then((items) => {
         const updatedOptions = {};
-        if (optSfmEnabledGlobalKey_v0_2_1 in items) {
-            const sfmEnabledGlobal = items[optSfmEnabledGlobalKey_v0_2_1];
-            log("Migrating option [%s] from [%s=%o] to [%s=%o]", OPT_SFM_ENABLED_GLOBAL_NAME, optSfmEnabledGlobalKey_v0_2_1, sfmEnabledGlobal, OPT_SFM_ENABLED_GLOBAL_NAME, sfmEnabledGlobal);
+        if (optSfmEnabledGlobalKey_pre_v0_3_0 in items) {
+            const sfmEnabledGlobal = items[optSfmEnabledGlobalKey_pre_v0_3_0];
+            log("Migrating option [%s] from [%s=%o] to [%s=%o]", OPT_SFM_ENABLED_GLOBAL_NAME, optSfmEnabledGlobalKey_pre_v0_3_0, sfmEnabledGlobal, OPT_SFM_ENABLED_GLOBAL_NAME, sfmEnabledGlobal);
             updatedOptions[OPT_SFM_ENABLED_GLOBAL_NAME] = sfmEnabledGlobal;
         }
-        if (optSfmEnabledChannelsKey_v0_2_1 in items) {
+        if (optSfmEnabledChannelsKey_pre_v0_3_0 in items) {
             // Channels were stored as an Array<String> of qualified names
-            const channelQualifiedNames = items[optSfmEnabledChannelsKey_v0_2_1];
+            const channelQualifiedNames = items[optSfmEnabledChannelsKey_pre_v0_3_0];
             const channels = Channel.parseArrayFromQualifiedNames(channelQualifiedNames);
             const serializedChannels = Channel.serializeArray(channels);
-            log("Migrating option [%s] from [%s=%o] to [%s=%o]", OPT_SFM_ENABLED_CHANNELS_NAME, optSfmEnabledChannelsKey_v0_2_1, channelQualifiedNames, OPT_SFM_ENABLED_CHANNELS_NAME, serializedChannels);
+            log("Migrating option [%s] from [%s=%o] to [%s=%o]", OPT_SFM_ENABLED_CHANNELS_NAME, optSfmEnabledChannelsKey_pre_v0_3_0, channelQualifiedNames, OPT_SFM_ENABLED_CHANNELS_NAME, serializedChannels);
             updatedOptions[OPT_SFM_ENABLED_CHANNELS_NAME] = serializedChannels;
         }
         return updatedOptions;
     }).then((updatedOptions) => {
-        return Promise.all([updatedOptions, opnd.browser.removeOptions(optKeys_v0_2_1)]);
+        return Promise.all([updatedOptions, opnd.browser.removeOptions(optKeys_pre_v0_3_0)]);
     }).then(([updatedOptions,]) => {
         return opnd.browser.writeOptions(updatedOptions);
     });
