426a62aad6d1dee4190ba8b453f704f2dad1205c
snowwits <snowwits@gmail.com>
2019-06-25 19:39:12 +0200

    Implemented real manifest version comparison for migration on update ♻️

--- browser_extension/common/common.js
+++ browser_extension/common/common.js
@@ -1215,6 +1215,64 @@ function extractDurationParts(duration) {
 
 /*
  * ====================================================================================================
+ * SEMANTIC VERSION
+ * ====================================================================================================
+ */
+
+/**
+ * Compares two manifest versions (see https://developer.chrome.com/extensions/manifest/version).
+ *
+ * @param {string} v1 the first version to be compared
+ * @param {string} v2 the second version to be compared
+ * @returns {number|NaN}
+ * <ul>
+ *    <li>0 if the versions are equal</li>
+ *    <li>a negative integer if v1 < v2</li>
+ *    <li>a positive integer if v1 > v2</li>
+ *    <li>NaN if either version string is in the wrong format</li>
+ * </ul>
+ *
+ * @author http://stackoverflow.com/a/6832721
+ */
+function compareManifestVersions(v1, v2) {
+    if (typeof v1 !== "string" || typeof v2 !== "string") {
+        warnWithComponent("common", "Could not compare manifest versions because either is not a string: [$s] [$s]", v1, v2);
+        return NaN;
+    }
+
+    const v1parts = v1.split('.');
+    const v2parts = v2.split('.');
+
+    const isValidPart = (x) => /^\d+$/.test(x);
+
+    if (!v1parts.every(isValidPart) || !v2parts.every(isValidPart)) {
+        warnWithComponent("common", "Could not compare manifest versions because either is syntactically incorrect: [$s] [$s]", v1, v2);
+        return NaN;
+    }
+
+    // Pad with zero parts if parts are not equally long
+    // E.g. pad "1" to "1.0.0" if compared with "1.0.1"
+    while (v1parts.length < v2parts.length) v1parts.push("0");
+    while (v2parts.length < v1parts.length) v2parts.push("0");
+
+    const v1partsNums = v1parts.map(Number);
+    const v2partsNums = v2parts.map(Number);
+
+    for (let i = 0; i < v1partsNums.length; ++i) {
+        if (v1partsNums[i] === v2partsNums[i]) {
+            // continue
+        } else if (v1partsNums[i] > v2partsNums[i]) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+/*
+ * ====================================================================================================
  * WINDOW UTILS
  * ====================================================================================================
  */
